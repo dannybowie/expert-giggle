6d01805d4cb5dbcaa33d00bf0697de8866059a5a
danny <kremzan17@gmail.com>
2025-07-04 17:36:26 -0400

    env fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,15 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDpzLlEPWKJJMamLcvsrCHrloahOX61-y0",
-  authDomain: "authoring-project.firebaseapp.com",
-  projectId: "authoring-project",
-  storageBucket: "authoring-project.firebasestorage.app",
-  messagingSenderId: "827234552480",
-  appId: "1:827234552480:web:616fa768e4cd465f2757c5",
-  measurementId: "G-EC8QX27Q03"
-};
-
+// src/environments/environment.ts
 export const environment = {
   production: false,
-  firebase: firebaseConfig
+  firebase: {
+    apiKey: process.env['FIREBASE_API_KEY'] || '',
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
+    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
+    appId: process.env['FIREBASE_APP_ID'] || ''
+  }
 };
