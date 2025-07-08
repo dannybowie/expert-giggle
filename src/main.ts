ae01a6be88bb52ccc0f73f894c5fabef8e915f9e
danny bowie <kremzan17@gmail.com>
2025-07-08 07:56:35 -0400

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes'; // Make sure this path is correct
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Hard-coded Firebase config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
+}).catch(err => console.error('Error bootstrapping application', err));
