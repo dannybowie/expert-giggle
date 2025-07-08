4dcd5c40d24d9a8f06adb6a22a976adc55e3f33f
danny bowie <kremzan17@gmail.com>
2025-07-08 07:47:39 -0400

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,12 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
-import { environment } from '../environments/environment';
+//import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
   ]
